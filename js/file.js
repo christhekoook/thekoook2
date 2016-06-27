85cbfeacc95e5e748ac7870192a004a34060ca5d
christhekoook <chrisgutierrez@chriss-mbp.attlocal.net>
2016-06-27 17:08:56 -0500

    initial

---
+++ js/file.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+$("h1").hide().fadeIn(2000);
+$("img").hide().fadeIn(1500);
+$("a").hide().slideDown(3000);
+
+
+
+$(".backbutton").on("click", function(){
+window.location.href='index.html';
+});
+
+
+});
